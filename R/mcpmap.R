6c833abb0112400e8b68d4b8c14827f1f418d11a
ielbabisy <elbadisyimad@gmail.com>
2024-06-04 23:43:51 +0100

    first commit test

--- R/mcpmap.R
+++ R/mcpmap.R
@@ -1,31 +1,33 @@
-#' Parallel map over a list using multiple cores
+#' Parallel Map Over a List Using Multiple Coers
 #'
-#' Applies a function in parallel over elements of a list of elements with the same length with support for multiple cores.
-#' The function uses `parallel::mcmapply` under the hood to perform the parallel computation, automatically determining the number
-#' of cores to use if not specified.
+#' This function applies a given function in parallel over elements of a list. It uses `parallel::mcmapply` on Unix-like
+#' systems and `parallel::parLapply` on Windows. This approach ensures cross-platform compatibility by automatically
+#' adjusting to the appropriate method of parallel execution depending on the operating system.
 #'
-#' @param lists A list of lists containing the arguments to pass to the function `func`. Each inner list should
-#' correspond to an argument of `func`, and all lists must be of the same length.
-#' @param func The function to apply to each set of arguments contained in `lists`.
-#' @param num_cores The number of cores to use for parallel execution. The default is
-#' one less than the total number of cores available on the system to leave resources
-#' for other processes.
-#' @return The result of applying `func` to the elements of `lists` in parallel. The return value is a list of the
-#' same length as the input lists, where each element is the result of applying `func` to the corresponding elements
-#' of the input lists.
+#' @param lists A list of lists where each inner list contains the arguments to be passed to the function `func`.
+#'              Each inner list should correspond to an argument of `func`, and all lists must have the same length.
+#' @param func The function to apply to each set of arguments contained in `lists`. The function should be capable
+#'             of handling input as specified in `lists`.
+#' @param num_cores The number of cores to use for parallel execution. This defaults to one less than the total number
+#'                  of cores available on the system, leaving resources free for other processes.
+#' @return A list of results, where each element is the result from applying `func` to the corresponding elements of
+#'         the input lists. The length of the return list is the same as the length of the input lists.
 #' @examples
-#' # Define a function to apply
+#' # Define a function to be applied
 #' sum_func <- function(x, y) x + y
 #'
 #' # Create a list of arguments
-#' args_list <- list(c(1,2,3), c(4,5,6))
+#' args_list <- list(c(1,2,3), to = c(4,5,6))
 #'
 #' # Apply the function in parallel
 #' results <- mcpmap(args_list, sum_func, num_cores = 2)
 #' print(results)
 #' @export
-#' @importFrom parallel mcmapply detectCores
-#'
+#' @importFrom parallel detectCores mcmapply makeCluster stopCluster clusterExport parLapply
+#' @keywords parallel
+#' @note On Windows systems, since `mcmapply` is not supported due to the lack of forking ability, this function
+#'       uses a parallel socket cluster (`parLapply`) instead. Users are advised that parallel execution on Windows
+#'       involves creating and managing a cluster, which might introduce some overhead compared to forking on Unix-like systems.
 mcpmap <- function(lists, func, num_cores = parallel::detectCores()-1) {
   stopifnot(is.list(lists))  # Ensure that input is a list of lists
 
@@ -34,7 +36,30 @@ mcpmap <- function(lists, func, num_cores = parallel::detectCores()-1) {
     stop("All elements of the list must have the same length")
   }
 
-  do.call(parallel::mcmapply, c(list(FUN = func, MoreArgs = NULL, SIMPLIFY = FALSE, USE.NAMES = FALSE, mc.cores = num_cores), lists))
+  is_windows <- .Platform$OS.type == "windows"
+
+  if (is_windows) {
+    # On Windows, use parLapply with a PSOCK cluster
+    cl <- parallel::makeCluster(num_cores)
+    on.exit(parallel::stopCluster(cl), add = TRUE)  # Ensure cluster is stopped when function exits
+
+    # Prepare list of arguments for each call
+    args <- transpose(lists)
+
+    # Export the function and any required variables
+    parallel::clusterExport(cl, varlist = "func", envir = environment())
+
+    # Apply function over the list using parLapply
+    results <- parallel::parLapply(cl, args, function(x) do.call(func, x))
+  } else {
+    # On Unix-like systems, use mcmapply
+    results <- do.call(parallel::mcmapply, c(list(FUN = func, MoreArgs = NULL, SIMPLIFY = FALSE, USE.NAMES = FALSE, mc.cores = num_cores), lists))
+  }
+
+  return(results)
 }
 
-
+# Helper function to transpose a list of vectors into a list of lists for each combination
+transpose <- function(lst) {
+  mapply(function(...) list(...), lst, SIMPLIFY = FALSE)
+}
