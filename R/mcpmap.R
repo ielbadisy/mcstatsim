db6e349f877dc028816b85386c599746fe8d78f1
ielbabisy <elbadisyimad@gmail.com>
2024-06-10 10:23:55 +0100

    re-roxygenize all

--- R/mcpmap.R
+++ R/mcpmap.R
@@ -1,33 +1,25 @@
-#' Parallel map over a list using multiple cores
+#' Parallel Map Over Lists Using Multiple Cores
 #'
-#' Applies a function in parallel over elements of a list with the same length with support for multiple cores.
-#' The function uses `parallel::mcmapply` under the hood to perform the parallel computation, automatically determining the number
-#' of cores to use if not specified. On Windows systems, the number of cores is automatically set to 1 due to limitations
-#' in parallel processing support.
+#' @description This function applies a given function in parallel over elements of a list.
+#' It uses `parallel::mcmapply` on Unix-like systems and `parallel::parLapply` on Windows.
+#' This approach ensures cross-platform compatibility.
 #'
-#' @param lists A list of lists containing the arguments to pass to the function `func`. Each inner list should
-#' correspond to an argument of `func`, and all lists must be of the same length.
+#' @param lists A list of lists where each inner list contains the arguments to be passed to the function `func`.
+#' Each inner list should correspond to an argument of `func`, and all lists must have the same length.
 #' @param func The function to apply to each set of arguments contained in `lists`.
-#' @param num_cores The number of cores to use for parallel execution. The default is
-#' one less than the total number of cores available on the system to leave resources
-#' for other processes. Note: On Windows, `num_cores` is automatically set to 1.
-#' @return The result of applying `func` to the elements of `lists` in parallel. The return value is a list of the
-#' same length as the input lists, where each element is the result of applying `func` to the corresponding elements
+#' @param num_cores The number of cores to use for parallel execution, defaulting to the number of cores
+#' available on the system minus one. On Windows, due to lack of support for multicore execution using
+#' forking, the default is set to 1.
+#'
+#' @return A list of results, each element being the result from applying `func` to the corresponding elements
 #' of the input lists.
 #' @examples
-#' # Define a function to apply
 #' sum_func <- function(x, y) x + y
-#'
-#' # Create a list of arguments
-#' args followed by the function argument.
-#' args_list <- list(c(1,2,3), c(4,5,6))
-#'
-#' # Apply the function in parallel
+#' args_list <- list(c(1, 2, 3), c(4, 5, 6))
 #' results <- mcpmap(args_list, sum_func, num_cores = 2)
-#' print(results)
 #' @export
-#' @importFrom parallel mcmapply detectCores
-#'
+#' @importFrom parallel detectCores mcmapply parLapply makeCluster stopCluster clusterExport
+#' @seealso \code{\link[parallel]{mcmapply}}, \code{\link[parallel]{parLapply}}
 mcpmap <- function(lists, func, num_cores = parallel::detectCores() - 1) {
   stopifnot(is.list(lists))  # Ensure that input is a list of lists
 
