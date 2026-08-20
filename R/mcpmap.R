c5b09e3b11c9467d5e94652470e413a5f9a42991
ielbadisy <elbadisyimad@gmail.com>
2026-08-20 15:24:06 +0100

    Use functionals::fmapn as the parallel-map backend
    Replace pbapply::pbmapply in mcpmap() with functionals::fmapn(),
    which provides the same named-list-of-equal-length-vectors mapping
    semantics with sequential/multicore/cluster execution and an optional
    progress bar. runsim() is unaffected since mcpmap()'s signature and
    behavior are unchanged.

--- R/mcpmap.R
+++ R/mcpmap.R
@@ -1,4 +1,4 @@
-#' Parallel Map Function using pbapply::pbmapply
+#' Parallel Map Function using functionals::fmapn
 #'
 #' This function applies a given function over a list of parameters in parallel using multiple cores.
 #'
@@ -7,12 +7,11 @@
 #' @param num_cores The number of cores to use for parallel execution. Default is one less than the total number of available cores.
 #' @param show_progress Logical indicating whether to display the progress bar. Default is TRUE.
 #' @return A list of results from applying the function over the parameters.
-#' @details The function ensures that all elements in the list have the same length and uses `pbapply::pbmapply` for parallel processing.
+#' @details The function ensures that all elements in the list have the same length and uses `functionals::fmapn` for parallel processing.
 #' It sets the number of cores based on the operating system and then applies the function in parallel.
 #' @examples
 #' params <- list(a = 1:3, b = 4:6)
 #' mcpmap(params, function(a, b) a + b, num_cores = 2)
-#' @importFrom pbapply pbmapply pboptions
 #' @export
 mcpmap <- function(lists, func, num_cores = parallel::detectCores() - 1, show_progress = TRUE) {
   stopifnot(is.list(lists))  # Ensure that input is a list of lists
@@ -22,11 +21,5 @@ mcpmap <- function(lists, func, num_cores = parallel::detectCores() - 1, show_pr
     stop("All elements of the list must have the same length")
   }
 
-  if (show_progress) {
-    pbapply::pboptions(type = "timer", use_lb = FALSE, nout = num_cores, char = "=")
-  } else {
-    pbapply::pboptions(type = "none")
-  }
-
-  do.call(pbapply::pbmapply, c(list(FUN = func, MoreArgs = NULL, SIMPLIFY = FALSE, USE.NAMES = FALSE), lists))
+  functionals::fmapn(lists, func, ncores = num_cores, pb = show_progress)
 }
